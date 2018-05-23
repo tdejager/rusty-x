9e3efacb6a179e567bdae0409bff8a3c66e41e77
Tim de Jager <tdejager@smart-robotics.nl>
2018-05-23 16:32:40 +0200

    feat: created syntax highlighting

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,9 @@
 extern crate clap;
+
 use clap::{Arg, App, SubCommand};
 
 extern crate x;
+
 use x::{start_operation, Error, OpCode, Project};
 
 use std::io;
@@ -12,21 +14,49 @@ use std::path;
 use std::fs::File;
 use std::io::Read;
 use std::io::Write;
+use std::io::BufRead;
+
+extern crate syntect;
+
+use syntect::parsing::SyntaxSet;
+use syntect::easy::HighlightFile;
+use syntect::highlighting::{Theme, ThemeSet, Style};
+use syntect::util::as_24_bit_terminal_escaped;
+
+fn display_snippet(full_path: &path::PathBuf) {
+    let ss = SyntaxSet::load_defaults_newlines();
+    let ts = ThemeSet::load_defaults();
+    let theme = ts.themes.get("base16-ocean.dark");
+
+    let mut highlighter = HighlightFile::new(full_path, &ss, theme.unwrap()).unwrap();
+    let mut line = String::new();
+    while highlighter.reader.read_line(&mut line).unwrap() > 0 {
+        {
+            let regions: Vec<(Style, &str)> = highlighter.highlight_lines.highlight(&line);
+            print!("{}", as_24_bit_terminal_escaped(&regions[..], true));
+        }
+
+        line.clear();
+    }
+
+    // Clear the formatting
+    println!("\x1b[0m");
+}
 
 fn main() -> Result<(), Error> {
     let matches = App::new("Rusty X")
-                          .version("0.1")
-                          .author("Tim de Jager <tdejager89@gmail.com>")
-                          .about("Rusty snippet manager")
+        .version("0.1")
+        .author("Tim de Jager <tdejager89@gmail.com>")
+        .about("Rusty snippet manager")
         .arg(Arg::with_name("KEYWORDS")
-                               .help("Keywords to search for")
-                               .required(true)
-                               .multiple(true))
+            .help("Keywords to search for")
+            .required(true)
+            .multiple(true))
         .subcommand(SubCommand::with_name("add")
             .about("Add a snippet")
             .arg(Arg::with_name("filename")
-                     .help("Snippet file name")
-                     .required(true)))
+                .help("Snippet file name")
+                .required(true)))
         .get_matches();
 
     // Should add
@@ -45,19 +75,18 @@ fn main() -> Result<(), Error> {
     let project = Project::default_project();
     match res {
         Err(err) =>
-             {
-                 Err(err)
-             }
+            {
+                Err(err)
+            }
         Ok(snippets) =>
             {
                 for snip in &snippets {
                     let full_path = path::Path::new(&project.folder_name).join(snip.name.to_owned());
-
-                    let mut contents = fs::read_to_string(&full_path)?;
-                    println!("{:?}", contents);
+                    display_snippet(&full_path);
+                    //let mut contents = fs::read_to_string(&full_path)?;
+                    //println!("{:?}", contents);
                 }
                 Ok(())
             }
     }
-    
 }
